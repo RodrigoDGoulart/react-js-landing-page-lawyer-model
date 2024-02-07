9a4a7d7fb68dfa038d115e34530f94dc3e3b5c9e
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-02-07 17:37:00 -0300

    feat: insert contact banner

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -35,31 +35,47 @@ function Navbar() {
   }, []);
   return (
     <>
-      <IconContext.Provider value={{ color: "#fff" }}>
-        <Nav>
-          <NavbarContainer>
-            <NavLogo to={config.nav.logo.url}>
-              <NavIcon
-                src={require(`../../images/${widthScreen < 500 ? config.nav.logo_mobile.src : config.nav.logo.src}`)}
-                alt={config.nav.logo.name}
-              />
-              {config.nav.logo.name}
-            </NavLogo>
-            <HamburgerIcon onClick={handleClick}>
-              {click ? <FaTimes /> : <FaBars />}
-            </HamburgerIcon>
-            <NavMenu onClick={handleClick} click={click}>
-              {config.nav.links.map((link) => (
-                <NavItem>
-                  <NavLinks to={link.url} onClick={closeMobileMenu}>
-                    {link.label}
-                  </NavLinks>
-                </NavItem>
-              ))}
-            </NavMenu>
-          </NavbarContainer>
-        </Nav>
-      </IconContext.Provider>
+        <IconContext.Provider value={{ color: "#fff" }}>
+          <div
+            style={{
+              color: "white",
+              backgroundColor: "#3bb100",
+              textAlign: "center",
+              fontSize: "1em",
+              paddingBlock: "8px",
+            }}
+          >
+            Gostou do modelo? Entre em contato conosco para fazer o seu!
+            Contato: {`(12) 99669-3634`}
+          </div>
+          <Nav>
+            <NavbarContainer>
+              <NavLogo to={config.nav.logo.url}>
+                <NavIcon
+                  src={require(`../../images/${
+                    widthScreen < 500
+                      ? config.nav.logo_mobile.src
+                      : config.nav.logo.src
+                  }`)}
+                  alt={config.nav.logo.name}
+                />
+                {config.nav.logo.name}
+              </NavLogo>
+              <HamburgerIcon onClick={handleClick}>
+                {click ? <FaTimes /> : <FaBars />}
+              </HamburgerIcon>
+              <NavMenu onClick={handleClick} click={click}>
+                {config.nav.links.map((link) => (
+                  <NavItem>
+                    <NavLinks to={link.url} onClick={closeMobileMenu}>
+                      {link.label}
+                    </NavLinks>
+                  </NavItem>
+                ))}
+              </NavMenu>
+            </NavbarContainer>
+          </Nav>
+        </IconContext.Provider>
     </>
   );
 }
